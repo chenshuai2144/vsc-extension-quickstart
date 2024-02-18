02f4b3ece809162c2798abc6d0a4a96282dadf31
期贤 <qixian.cs@antgroup.com>
2024-02-18 18:56:32 +0800

    优化使用场景

--- src/props-hover.ts
+++ src/props-hover.ts
@@ -1,8 +1,9 @@
 ﻿import path from 'path';
 import fs from 'fs';
 import { Position, TextDocument } from 'vscode';
-import { slash } from './util';
+import { slash, getProjectPath } from './util';
 import * as vscode from 'vscode';
+
 /**
  * 鼠标悬停提示，当鼠标停在package.json的dependencies或者devDependencies时，
  * 自动显示对应包的名称、版本号和许可协议
@@ -12,13 +13,14 @@ import * as vscode from 'vscode';
  */
 export function provideHover(document: TextDocument, position: Position) {
   const fileName = slash(document.fileName);
-  const workDir = path.dirname(fileName);
+  const workDir = getProjectPath(document);
   const word = document.getText(document.getWordRangeAtPosition(position));
-  const packageNameRegex = /"([^"]+)":/;
-  const line = document.lineAt(position);
-  const matches = line.text.match(packageNameRegex) || [];
-  const packageName = matches.length > 1 ? matches?.[1] : word;
+
   if (/\/package\.json$/.test(fileName)) {
+    const packageNameRegex = /"([^"]+)":/;
+    const line = document.lineAt(position);
+    const matches = line.text.match(packageNameRegex) || [];
+    const packageName = matches.length > 1 ? matches?.[1] : word;
     let destPath = `${workDir}/node_modules/${packageName.replace(
       /"/g,
       ''
@@ -31,4 +33,19 @@ export function provideHover(document: TextDocument, position: Position) {
       );
     }
   }
+
+  const relativePath = slash(path.relative(workDir, fileName));
+  if (['config/config.ts', '.umirc.ts'].includes(relativePath)) {
+    const apiJson = require('@ant-design/doc/api/umi.json');
+
+    let api = apiJson
+      ?.find((item: any) => item.title === '配置')
+      ?.properties?.find?.((item: any) => item.title === word);
+
+    if (api) {
+      return new vscode.Hover(api.md);
+    }
+  }
+
+  return null;
 }
